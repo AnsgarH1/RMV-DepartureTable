4a7eb6f5457dd141b43a1883e19da1deaa7433f7
AnsgarH1 <46782109+AnsgarH1@users.noreply.github.com>
2019-01-19 02:58:48 +0100

    removed API Key

--- php/getSearchResult.php
+++ php/getSearchResult.php
@@ -1,7 +1,7 @@
 <?php
     
     $input = $_GET["content"];
-	$baseUrl = "https://www.rmv.de/hapi/location.name?accessId=2e275638-45cb-4f79-b647-dcbbfb7a76e2&format=json&type=S&input=";
+	$baseUrl = "https://www.rmv.de/hapi/location.name?accessId=INSERTYOURAPIKEYHERE&format=json&type=S&input=";
     
     $url = $baseUrl. $input;
     
